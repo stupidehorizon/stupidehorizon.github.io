e8ef7fbb4e5478b0094d065d400fd827882e6a58
stupidehorizon <122505231@qq.com>
2017-03-11 21:35:29 +0800

    Site updated: 2017-03-11 21:35:25

--- muke/js/mf-pattern/mF_classicHB.js
+++
@@ -1,9 +0,0 @@
-myFocus.pattern.extend({
-	'mF_classicHB':function(settings,$){//*********************经典怀旧系列二--海报风格******************
-		var $focus=$(settings);
-		var $picList=$focus.find('.pic li');
-		var $txtList=$focus.addListTxt().find('li');
-		var $numList=$focus.addListNum().find('li');
-		//CSS
-		var w=settings.width,h=settings.height,txtH=settings.txtHeight;//设置默认的文字高度
-		for(var i=0,n=$picList.length;i
