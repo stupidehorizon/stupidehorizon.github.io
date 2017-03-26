766da836e4e0348144a38d372e472fe7f8352f01
stupidehorizon <122505231@qq.com>
2017-03-26 16:14:03 +0800

    Site updated: 2017-03-26 16:13:58

---
+++ aboutme/pageswitch/pageswitch-jq.js
@@ -0,0 +1,53 @@
+(function($){
+	
+	//设置默认参数
+	defaults={
+		selectors:{                 //class指定样式类
+			sections:".sections",   //父元素样式类
+			section:".section",     //分页元素样式类
+			page:".pages",		    //换页元素样式类
+			active:".active"        //被选中的换页元素样式类
+		},
+		index:0,                     //页面开始索引
+		easing:'ease',               //动画效果
+		duration:500,				 //动画执行时间
+		loop:true,					//是否循环切换
+		pagination:true,			//是否进行分页
+		keyboard:true,				//是否触发键盘事件 暂时未定义
+		direction:"horizontal",	    //滑动方向 vertical horizontal
+		callback:"",                 //回调函数
+		auto:true                    //是否自动轮播
+	};
+
+	//创建类 并且传入参数
+	var pageSwitch=(function(){
+		//创建类
+		function pageSwitch(element,options){
+			this.settings=$.extend(true,defaults,options||{});
+			this.element=$(element);         //取得包裹元素的引用
+			this.init();                     //调用原型方法的init方法确保该执行的函数的执行
+		}
+        //定义原型方法
+		pageSwitch.prototype={
+			init:function(){
+				var me=this;                  //里面的this相当于$(this) 因为到时是jq对象直接引用
+				me.selectors=me.settings.selectors;
+				me.sections=me.element.find(me.selectors.sections);
+				me.section=me.sections.find(me.selectors.section);
+
+				me.direction=me.settings.direction=="vertical"?true:false;
+				me.pageCount=me.pageCount();			
+				me.index=(me.settings.index>=0&&me.settings.index<me.pagecount)?me.settings.index:0; me.avtiveclass="me.selectors.active.substring(1);" me.canscroll="true;" if(!me.direction){="" me._initlayout();="" }="" if(me.settings.pagination){="" me._initpaging();="" me._initevent();="" 相当于调用了方法="" 此处不调用也可在后面写自调用方法="" me._autopaging();="" 这样说的话="" 谁又调用了init呢？？="" },="" 在原型外调用的="" 计算页面数量="" pagecount:function(){="" return="" this.section.length;="" 计算页面的长或宽="" switchlength:function(){="" this.direction="=1?this.element.height():this.element.width();" 上一页="" prev:function(){="" var="" me="this;" if(me.index="">0){
+					me.index--;
+				}else if(me.settings.loop){
+					me.index=me.pageCount-1;
+				}
+				me._scrollPage();
+			},
+            //下一页
+			next:function(){
+				var me=this;
+				if(me.index<me.pagecount){ me.index++;="" }else="" if(me.settings.loop){="" me.index="0;" }="" me._scrollpage();="" },="" 初始化页面布局="" _initlayout:function(){="" var="" me="this;" if(!me.direction){="" me.section.css({"float":"left","width":math.floor(100="" me.pagecount)+"%"});="" me.sections.css("width",100*me.pagecount+"%");="" 初始化分页标签="" _initpaging:function(){="" pagesclass="me.selectors.page.substring(1);" pagehtml="<ul class=" +pagesclass+"="">";
+				for(var i=0;i<me.pagecount;i++){ pagehtml+="<li></li>" ;="" }="" me.element.append(pagehtml);="" var="" pages="me.element.find(me.selectors.page);" me.pageitem="pages.find("li");" me.pageitem.eq(me.index).addclass(me.avtiveclass);="" console.log(me.avtiveclass);="" if(me.direction){="" pages.addclass("vertical");="" }else{="" pages.addclass("horizontal");="" },="" 自动切换="" _autopaging:function(){="" me="this;" if(me.settings.auto){="" autopaging="$(document).ready(function(){" me.next();="" me._scrollpage();="" },4000);="" me.element.on("wheel="" click",function(e){="" clearinterval(autopaging);="" me._autopaging();="" });="" 初始化事件="" _initevent:function(){="" 滚轮事件="" me.element.on("wheel",function(e){="" e.preventdefault();="" 取消事件的默认动作。="" delta="e.originalEvent.wheelDelta;" if(me.canscroll){="" if(delta="">0){
+							me.prev();
+						}else if(delta</me.pagecount;i++){></me.pagecount){></me.pagecount)?me.settings.index:0;>
